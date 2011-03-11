f5382f27894def915fae7e1e7fde03356183e466
thomas <thomas@a4fac2f9-6b20-4951-ad45-1722492d1df3>
2011-03-11 10:04:40 +0000

    fix a bug into the path to scm directory

--- lib/scm.lib.sh
+++ lib/scm.lib.sh
@@ -6,11 +6,11 @@ if [ "${__SCM_WRAPPER__:-}" != 'Loaded' ]; then
 
   # Load repository library
   CONF_GET "REPOSITORY_TYPE"
-  if [ -z "$(/bin/ls -1 ${DIRNAME}/lib/repository/${REPOSITORY_TYPE}.lib.sh)" ]; then
+  if [ -z "$(/bin/ls -1 ${DIRNAME}/lib/scm/${REPOSITORY_TYPE}.lib.sh)" ]; then
     echo "ERROR: Unable to find repository ${REPOSITORY_TYPE}"
     exit 2
   fi
-  . "${DIRNAME}/lib/repository/${REPOSITORY_TYPE}.lib.sh"
+  . "${DIRNAME}/lib/scm/${REPOSITORY_TYPE}.lib.sh"
 
   # Wrapper function to list available repositories
   REPOSITORIES_LIST() {
