f7994f52ce4bdfdbda24e6f478f1913061a3dd74
thomas <thomas@a4fac2f9-6b20-4951-ad45-1722492d1df3>
2011-03-11 11:19:18 +0000

    be able to create svn repository

--- lib/scm.lib.sh
+++ lib/scm.lib.sh
@@ -4,7 +4,11 @@ if [ "${__SCM_WRAPPER__:-}" != 'Loaded' ]; then
 
   __SCM_WRAPPER__='Loaded'
 
+  # ---------------------------------------------------------------------------
+
+  #
   # Load repository library
+  #
   CONF_GET "REPOSITORY_TYPE"
   if [ -z "$(/bin/ls -1 ${DIRNAME}/lib/scm/${REPOSITORY_TYPE}.lib.sh)" ]; then
     echo "ERROR: Unable to find repository ${REPOSITORY_TYPE}"
@@ -12,7 +16,18 @@ if [ "${__SCM_WRAPPER__:-}" != 'Loaded' ]; then
   fi
   . "${DIRNAME}/lib/scm/${REPOSITORY_TYPE}.lib.sh"
 
+  # ---------------------------------------------------------------------------
+
+  #
+  # Wrapper function to create a new repository
+  #
+  REPOSITORY_CREATE() {
+    SCM_CREATE $*
+  }
+
+  #
   # Wrapper function to list available repositories
+  #
   REPOSITORIES_LIST() {
     SCM_LIST $*
   }
--- lib/scm/svn.lib.sh
+++ lib/scm/svn.lib.sh
@@ -4,9 +4,44 @@ if [ "${__SCM__:-}" != 'Loaded' ]; then
 
   __SCM__='Loaded'
 
-  SCM_LIST() {
+  # ---------------------------------------------------------------------------
 
-    CONF_GET "SVN_PARENT_PATH"
+  #
+  # Load configuration parameters
+  #
+  CONF_GET "SVN_PARENT_PATH"
+
+  #
+  # Test parameters
+  #
+  if [ -z "${SVN_PARENT_PATH}" ]; then
+    echo "ERROR: configuration parameter not defined: SVN_PARENT_PATH"
+    exit 2
+  fi
+
+  #
+  # Auto calculate some parameters
+  #
+  SVN_PERMISSION="$(stat -c %U:%G ${SVN_PARENT_PATH})"
+
+  # ---------------------------------------------------------------------------
+
+  # Function to create a new repository
+  SCM_CREATE() {
+    local name=
+
+    name="$1"
+
+    if [ -n "$(/bin/ls -1 "${SVN_PARENT_PATH}" | grep ${name})" ]; then
+      MESSAGE "Repository already exists"
+    else
+      svnadmin create "${SVN_PARENT_PATH}/${name}"
+      chown -R ${SVN_PERMISSION} "${SVN_PARENT_PATH}/${name}"
+    fi
+  }
+
+  # Function to list available repositories
+  SCM_LIST() {
     /bin/ls -1 "${SVN_PARENT_PATH}"
   }
 
