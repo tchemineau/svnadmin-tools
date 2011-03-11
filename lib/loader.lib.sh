0fc182ac1fe1ec2f6484a5ad309db8f7760cea8a
thomas <thomas@a4fac2f9-6b20-4951-ad45-1722492d1df3>
2011-03-11 10:01:15 +0000

    rename into a shorter name

--- lib/loader.lib.sh
+++ lib/loader.lib.sh
@@ -100,7 +100,7 @@ if [ "${__LOADER__:-}" != 'Loaded' ]; then
   # Load libraries
   #
 
-  SCRIPT_HELPER_DIRECTORY="${DIRNAME}/lib/ScriptHelper"
+  SCRIPT_HELPER_DIRECTORY="${DIRNAME}/lib/helper"
   LOAD __LIB_ASK__  "${SCRIPT_HELPER_DIRECTORY}/ask.lib.sh"
   LOAD __LIB_CLI__  "${SCRIPT_HELPER_DIRECTORY}/cli.lib.sh"
   LOAD __LIB_CONF__ "${SCRIPT_HELPER_DIRECTORY}/conf.lib.sh"
